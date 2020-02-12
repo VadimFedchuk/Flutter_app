0e2086f9e97fe4993342c4599d2efcc3aea0f536
VadimFedchuk <vadimfedchuk1994@gmail.com>
2020-02-12 15:23:28 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -82,15 +82,166 @@ class NewsBox extends StatelessWidget {
   }
 }
 
+class MyBody extends StatefulWidget {
+
+  @override
+  createState() => new MyBodyState();
+}
+
+class MyBodyState extends State<MyBody> {
+
+  List<String> _array = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(itemBuilder: (context, i){
+      print('num $i : нечетное = ${i.isOdd}');
+
+      if(i.isOdd) return new Divider();
+
+      final int index = i ~/ 2;
+
+      print('index $index');
+      print('length ${_array.length}');
+      
+      if(index >= _array.length) _array.addAll(['$index', '${index + 1}', '${index + 2}']);
+      return new ListTile(title: new Text(_array[index]));
+    });
+  }
+}
+
+class MyForm extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => MyFormState();
+}
+
+class MyFormState extends State {
+  final _formKey = GlobalKey<FormState>();
+  GenderList _gender;
+  bool _agreement = false;
+
+  Widget build(BuildContext context) {
+    return Container(padding: EdgeInsets.all(10.0), child: new Form(key: _formKey,
+        child: new Column(children: <Widget>[
+          new Text('Имя пользователя:', style: TextStyle(fontSize: 20.0),),
+          new TextFormField(validator: (value){
+            if (value.isEmpty) return 'Пожалуйста введите свое имя';
+          }),
+
+          new SizedBox(height: 20.0),
+
+          new Text('Контактный E-mail:', style: TextStyle(fontSize: 20.0),),
+          new TextFormField(validator: (value){
+            if (value.isEmpty) return 'Пожалуйста введите свой Email';
+
+            String p = "[a-zA-Z0-9+.\_\%-+]{1,256}@[a-zA-Z0-9][a-zA-Z0-9-]{0,64}(.[a-zA-Z0-9][a-zA-Z0-9-]{0,25})+";
+            RegExp regExp = new RegExp(p);
+
+            if (regExp.hasMatch(value)) return null;
+
+            return 'Это не E-mail';
+          }),
+          
+          new SizedBox(height: 20.0),
+
+          new Text('Ваш пол: ', style: TextStyle(fontSize: 20.0)),
+
+          new RadioListTile(
+            title: const Text('Мужской'),
+            value: GenderList.male,
+            groupValue: _gender,
+            onChanged: (GenderList value) {setState(() { _gender = value;});},
+          ),
+
+          new RadioListTile(
+            title: const Text('Женский'),
+            value: GenderList.female,
+            groupValue: _gender,
+            onChanged: (GenderList value) {setState(() { _gender = value;});},
+          ),
+
+          new SizedBox(height: 20.0),
+          
+          new CheckboxListTile(value: _agreement, 
+              title: new Text('Я ознакомлен'+(_gender==null?'(а)':_gender==GenderList.male?'':'а')+' с '
+                  'документом "Согласие на обработку персональных данных" и даю согласие на обработку '
+                  'моих персональных данных в соответствии с требованиями "Федерального закона '
+                  'О персональных данных № 152-ФЗ".'),
+              onChanged: (bool value) => setState(() => _agreement = value)),
+          
+          new RaisedButton(onPressed: () {
+            if(_formKey.currentState.validate()) {
+              Color color = Colors.red;
+              String text;
+
+              if (_gender == null)
+                text = 'Выберите свой пол';
+              else if (_agreement == false)
+                text = 'Необходимо принять условия соглашения';
+              else {
+                text = 'Форма успешно заполнена';
+                color = Colors.green;
+              }
+
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text(text),
+                  backgroundColor: color));
+            }
+          }, child: Text('проверить'), color: Colors.blue, textColor: Colors.white)
+          
+        ])));
+  }
+}
+
+enum GenderList {male, female}
+
 void main() =>  runApp(
-    new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new Scaffold(
-            appBar: new AppBar(),
-            body: new NewsBox('Новый урок по Flutter', '''В новом уроке рассказывается, в каких случаях применять класс StatelessWidget и StatefulWidget. Приведены примеры их использования.''',
-                imageurl: 'https://flutter.su/favicon.png', num: 10)
-        )
-    )
+
+  new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text("форма ввода")),
+        body: new MyForm()
+      )
+  )
+
+//  new MaterialApp(
+//    debugShowCheckedModeBanner: false,
+//    home: new Scaffold(body: new Center(child: new MyBody()))
+//  )
+
+
+
+//  new MaterialApp(
+//      debugShowCheckedModeBanner: false,
+//      home: new Scaffold(
+//        body: new MyWidget(),
+//      )
+//  )
+
+
+//  new MaterialApp(
+//      debugShowCheckedModeBanner: false,
+//          home: new Scaffold(
+//            body: new ListView(children: <Widget>[
+//              new Text('0000'),
+//              new Divider(),
+//              new Text('0001'),
+//              new Divider(),
+//              new Text('0002')
+//            ])
+//          )
+//  )
+
+
+//    new MaterialApp(
+////        debugShowCheckedModeBanner: false,
+////        home: new Scaffold(
+////            appBar: new AppBar(),
+////            body: new NewsBox('Новый урок по Flutter', '''В новом уроке рассказывается, в каких случаях применять класс StatelessWidget и StatefulWidget. Приведены примеры их использования.''',
+////                imageurl: 'https://flutter.su/favicon.png', num: 10)
+////        )
+////    )
 );
 
 class MyWidget extends StatelessWidget {
